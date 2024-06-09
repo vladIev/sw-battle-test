32becdf08b472d58f2555f98d4c7bc3a0fd5204c
Владислав Левоцкий <vlad.levotskiy@gmail.com>
2024-06-09 11:34:08 +0300

    Satisfy One unit per cell requirment

--- src/Simulation/Map.cpp
+++ src/Simulation/Map.cpp
@@ -1,24 +1,27 @@
 #include <Simulation/Map.hpp>
+#include <cstdint>
 #include <types.hpp>
 
 #include <algorithm>
-#include <iterator>
+#include <set>
+#include <vector>
 
 namespace sw
 {
 
-auto Map::getUnitsInRange(Position center, uint32_t range) const -> std::vector<UnitId>
+auto Map::getUnitsInRange(Position center, uint32_t to, uint32_t from) const -> std::vector<UnitId>
 {
-	std::set<uint32_t> ys;
-	std::set<uint32_t> xs;
+	std::set<int64_t> ys;
+	std::set<int64_t> xs;
 
-	for(int64_t i = center.y - range; i <= center.y + range; i++)
+	for(int64_t i = center.y - to; i <= center.y + to; i++)
 	{
 		ys.insert(std::clamp(i, 0ll, static_cast<int64_t>(d_height - 1)));
 	}
 
-	for(int64_t i = center.x - range; i <= center.x + range; i++)
+	for(int64_t i = center.x - to; i <= center.x + to; i++)
 	{
+
 		xs.insert(std::clamp(i, 0ll, static_cast<int64_t>(d_width - 1)));
 	}
 
@@ -27,31 +30,26 @@ auto Map::getUnitsInRange(Position center, uint32_t range) const -> std::vector<
 	{
 		for(const auto x : xs)
 		{
-			std::ranges::copy(d_field[y][x], std::back_inserter(neighbourUnits));
+			if(std::abs(static_cast<int64_t>(x - center.x)) >= from &&
+			   std::abs(static_cast<int64_t>(y - center.y)) >= from)
+			{
+
+				const auto id = d_field[y][x];
+				if(id) neighbourUnits.push_back(id);
+			}
 		}
 	}
 
 	return neighbourUnits;
 }
 
-auto Map::getUnitsInRange(UnitId id, uint32_t range) const -> std::vector<UnitId>
+auto Map::getUnitsInRange(UnitId id, uint32_t range, uint32_t from) const -> std::vector<UnitId>
 {
 	assert(d_positions.contains(id));
 
 	const auto position = d_positions.at(id);
-	auto units = getUnitsInRange(position, range);
-	if(units.size() == 1)
-	{
-		units.clear();
-		return units;
-	}
-
-	auto selfIt = std::ranges::find(units, id);
-	if(selfIt != units.end())
-	{
-		*selfIt = *units.rbegin();
-		units.pop_back();
-	}
+	auto units = getUnitsInRange(position, range, from);
+	std::erase(units, id);
 	return units;
 }
 
--- src/Simulation/Map.hpp
+++ src/Simulation/Map.hpp
@@ -2,14 +2,14 @@
 #include <types.hpp>
 
 #include <cassert>
-#include <set>
+
 #include <unordered_map>
 
 namespace sw
 {
 class Map
 {
-	using Cell = std::set<UnitId>;
+	using Cell = UnitId;
 	using Field = std::vector<std::vector<Cell>>;
 	using UnitsPosition = std::unordered_map<UnitId, Position>;
 
@@ -18,7 +18,8 @@ class Map
 	const uint32_t d_width;
 	const uint32_t d_height;
 
-	[[nodiscard]] std::vector<UnitId> getUnitsInRange(Position center, uint32_t range) const;
+	[[nodiscard]] std::vector<UnitId>
+	getUnitsInRange(Position center, uint32_t to, uint32_t from) const;
 
   public:
 	Map(uint32_t width, uint32_t height)
@@ -34,7 +35,7 @@ class Map
 	{
 		assert(!d_positions.contains(id));
 
-		d_field[position.y][position.x].insert(id);
+		d_field[position.y][position.x] = id;
 		d_positions[id] = position;
 	}
 
@@ -43,7 +44,7 @@ class Map
 		assert(d_positions.contains(id));
 
 		const auto position = d_positions[id];
-		d_field[position.y][position.x].erase(id);
+		d_field[position.y][position.x] = 0;
 		d_positions.erase(id);
 	}
 
@@ -55,8 +56,8 @@ class Map
 		if(oldPosition == position) return;
 
 		d_positions[id] = position;
-		d_field[oldPosition.y][oldPosition.x].erase(id);
-		d_field[position.y][position.x].insert(id);
+		d_field[oldPosition.y][oldPosition.x] = 0;
+		d_field[position.y][position.x] = id;
 	}
 
 	[[nodiscard]] auto width() const noexcept
@@ -69,7 +70,8 @@ class Map
 		return d_height;
 	}
 
-	[[nodiscard]] auto getUnitsInRange(UnitId id, uint32_t range) const -> std::vector<UnitId>;
+	[[nodiscard]] auto
+	getUnitsInRange(UnitId id, uint32_t range, uint32_t from = 0) const -> std::vector<UnitId>;
 	[[nodiscard]] auto unitPosition(UnitId id) const -> Position;
 };
 } // namespace sw
